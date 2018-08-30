24e8a5fd16e3a561694fb6ebe587a41712dc96d7
Kamil Szczypior <kszczypior1@gmail.com>
2018-08-30 19:41:36 +0200

    Add ITCSS file structure

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+
+
+var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
+
+gulp.task('buildcss', function(){
+    return gulp.src('./dev-assets/style.scss')
+    .pipe(gulpSass())
+    .pipe(gulp.dest('./prod-assets'));
+});
+gulp.task('watch', function(){
+    gulp.watch('./dev-assets/**/*.scss', ['buildcss']);
+});
